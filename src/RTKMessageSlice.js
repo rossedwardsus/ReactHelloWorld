aea911c2c3b2abf01ff990cb77e9dbbb2990e367
Ross Edwards <rossedwards.us@gmail.com>
2023-06-28 17:45:54 -0400

    started adding delte and edit for items

--- src/RTKMessageSlice.js
+++ src/RTKMessageSlice.js
@@ -2,7 +2,7 @@ import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
  //colors generated
-  projects: [{projectId: "1", projectName: "pn", projectBudgetItems: [{projectItemDescription: "id", projectBudgetItemAmount: 100, projectItemAddedByUserId: 1, projectBudgetItemAddedDate: 1}]}],
+  projects: [{projectId: "1", projectName: "pn", projectBudgetItems: [{projectBudgetItemId: 1, projectItemDescription: "id", projectBudgetItemAmount: 100, projectItemAddedByUserId: 1, projectBudgetItemAddedDate: 1}]}],
 };
 
 //A function to generate random RGB values
@@ -23,10 +23,54 @@ export const messagesSlice = createSlice({
       //state.value = [...state.value, randomRgb()];
       state.value = [...state.value, action.payload];
     },
+    deleteProjectBudgetItem: (state, action) => {
+      //state.value = [...state.value, randomRgb()];
+      //find project index by projectId
+      //action.payload.projectId
+      const projectIndex = state.projects.findIndex(project => project.projectId == 1);
+      console.log(projectIndex + " " + JSON.stringify(state)); 
+      const projectBudgetItems = state.projects[projectIndex].projectBudgetItems;
+      //remove item from projectBudgetItemsList
+      //state.projects.projectBudgetItems
+      const itemIndex = projectBudgetItems.findIndex(item => item.projectBudgetItemId == 1);
+      //filter where itemId 1= 1
+      //slice/splice
+      projectBudgetItems.splice(itemIndex, 1);
+      //or just delte item at index
+      state.projects[0].projectBudgetItems = projectBudgetItems;
+      state.value = [...state.projects]
+
+      console.log(JSON.stringify(state));
+
+      //state.value = [...state.value, action.payload];
+      //state;
+    },
+    updateProjectBudgetItem: (state, action) => {
+      //state.value = [...state.value, randomRgb()];
+      //find project index by projectId
+      //action.payload.projectId
+      const projectIndex = state.projects.findIndex(project => project.projectId == 1);
+      console.log(projectIndex + " " + JSON.stringify(state)); 
+      const projectBudgetItems = state.projects[projectIndex].projectBudgetItems;
+      //remove item from projectBudgetItemsList
+      //state.projects.projectBudgetItems
+      const itemIndex = projectBudgetItems.findIndex(item => item.projectBudgetItemId == 1);
+      //filter where itemId 1= 1
+      //slice/splice
+      //projectBudgetItems.splice(itemIndex, 1);
+      //or just delte item at index
+      state.projects[0].projectBudgetItems = {...projectBudgetItems, amount: 100};
+      state.value = [...state.projects]
+
+      console.log(JSON.stringify(state));
+
+      //state.value = [...state.value, action.payload];
+      //state;
+    },
   },
 });
 
 // Action creators are automatically generated for each case reducer function 
-export const { setMessages } = messagesSlice.actions;
+export const { setMessages, deleteProjectBudgetItem } = messagesSlice.actions;
 
 export default messagesSlice.reducer;
